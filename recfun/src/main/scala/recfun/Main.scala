25a6f12b70ae783539bedc89fff79271f5ed9408
Blake Clough <blakeclough@outlook.com>
2017-06-09 16:51:48 -0700

    Got two functions working

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -11,32 +11,33 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
+    * Exercise 1
+    */
   def pascal(c: Int, r: Int): Int =
     if (c > r) 0
     else if (c == 0) 1
-    else return pascal(c - 1, r - 1) + pascal(c , r - 1)
-  
+    else return pascal(c - 1, r - 1) + pascal(c, r - 1)
+
   /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    val rightParen = '('
+    val leftParen = ')'
+
+    def isBalanced(remainingChars: List[Char], leftParens: Int): Boolean = {
+      if (remainingChars.isEmpty) leftParens == 0
+      else if (remainingChars.head == leftParen) isBalanced(remainingChars.tail, leftParens + 1)
+      else if (remainingChars.head == rightParen) leftParens > 0 && isBalanced(remainingChars.tail, leftParens - 1)
+      else isBalanced(remainingChars.tail, leftParens)
+    }
+    isBalanced(chars, 0)
+  }
+
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int =
+      if (coins.isEmpty || money == 0) 0
+      else 1
   }
-
-/*
-1
-1 1
-1 2 1
-1 3 3 1
-1 4 6 4 1
-
-
-
-
- */
